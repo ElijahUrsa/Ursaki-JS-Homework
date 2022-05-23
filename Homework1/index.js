cb0830f4ed8ae7406e40673314a7fe2936d3223f
Elijah <99914103+ElijahUrsa@users.noreply.github.com>
2022-05-23 11:03:41 +0200

    Update index.js

--- Homework1/index.js
+++ Homework1/index.js
@@ -4,48 +4,48 @@ alert('Illia');
 
 //task 3
 
-// let a = 'Andrew';
-// let x = 46;
-// alert(a);
-// alert(x);
-// x = a;
-// alert(x);
+let a = 'Andrew';
+let x = 46;
+alert(a);
+alert(x);
+x = a;
+alert(x);
 
 //task 4
 
-// let user = {
-//     Name: 'Andrew',
-//     Age: 26,
-//     Human: true,
-//     Phone: undefined,
-//     Job: null
-//     };
-//     console.log(user);
+let user = {
+    Name: 'Andrew',
+    Age: 26,
+    Human: true,
+    Phone: undefined,
+    Job: null
+    };
+    console.log(user);
 
 //task 5
 
-// let isAdult = confirm('Ви досягли повноліття?');
-// alert (isAdult);
+let isAdult = confirm('Ви досягли повноліття?');
+alert (isAdult);
 
 //task 6
 
-// let Name = 'Illia';
-// let Surname = 'Ursaki';
-// let StudyGroup = 'Lv-678.JS-Core';
-// let BirthYear = 2003;
-// let isEnabled = true;
+let Name = 'Illia';
+let Surname = 'Ursaki';
+let StudyGroup = 'Lv-678.JS-Core';
+let BirthYear = 2003;
+let isEnabled = true;
 
-// console.log(typeof Name);
-// console.log(typeof Surname);
-// console.log(typeof StudyGroup);
-// console.log(typeof BirthYear);
-// console.log(typeof isEnabled);
+console.log(typeof Name);
+console.log(typeof Surname);
+console.log(typeof StudyGroup);
+console.log(typeof BirthYear);
+console.log(typeof isEnabled);
 
-// let salary = null;
-// let job;
+let salary = null;
+let job;
 
-// console.log(typeof salary);
-// console.log(typeof job);
+console.log(typeof salary);
+console.log(typeof job);
 
 
 //task 7
